1a6580e5c6c1bea2135164beddae4447ca7e8b47
crookfion <crookfion@gs1608.AD.ECS.VUW.AC.NZ>
2018-08-08 15:59:04 +1200

    Hello - latest commit
    Unsure if any changes beyond comments

--- FC_HelloWorldEx/src/Hello.java
+++ FC_HelloWorldEx/src/Hello.java
@@ -14,17 +14,21 @@ public class Hello extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		//window title in top bar
 		primaryStage.setTitle("Hello World");
 		
-		final Text text=new Text();
+		//text generation
+		final Text text=new Text(); //need final to say will have text and not be null
 		text.setText("No-one has been greated");
 		
+		//button generation
 		Button btn=new Button();
 		btn.setText("Greet you");
+		//actions for the button
 		btn.setOnAction(new EventHandler<ActionEvent>() {
 			int n=0;
 			@Override
-			public void handle(ActionEvent arg0) {
+			public void handle(ActionEvent arg0) { //what we want to happen when button pressed
 				n++;
 				text.setText("Hello there! "+"Said "+n+" time(s)");
 				
@@ -32,19 +36,23 @@ public class Hello extends Application {
 			
 		});
 		
+		//setting internal window layout
 		HBox pane=new HBox();
 		pane.setAlignment(Pos.CENTER);
 		pane.setPadding(new Insets(25,25,25,25));
 		pane.setSpacing(10);
+		//adding text and button to layout
 		pane.getChildren().add(text);
 		pane.getChildren().add(btn);
 		
+		//adding pane (layout) to scene
 		primaryStage.setScene(new Scene(pane,300, 250));
 		primaryStage.show();
 		
 	}
 
 	public static void main(String[] args) {
+		//the bit that makes it work
 		launch(args);
 	}
 
